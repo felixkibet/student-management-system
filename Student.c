3b98bc077ccb834d65a7f50f951823e3b9c49c26
fellodurant <fellodurant97@gmail.com>
2019-03-19 22:15:40 +0300

    Created structure Student

--- Student.c
+++ Student.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 
+typedef struct Student Student;
+
+struct Student {
+	int regno;
+	char fname[10];
+	char lname[10];
+	char gender[2];
+	char course[10];
+};
+
 int main()
 {
     return 0;
